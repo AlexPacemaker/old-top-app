8dbb8574b64181ff35341fdaf4c5112acde25fa3
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-19 16:48:15 +0300

    global font added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
       <Head>
         <title>MyTop - наш лучший топ</title>
         <link rel='icon' href='/favicon.ico' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
       </Head>
       <Component {...pageProps} />;
     </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Document = (): JSX.Element => {
   return (
     <Html lang='ru'>
-      <Head />
+      <Head>
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;400;700&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
