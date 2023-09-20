19840446bafccc004580110cb1e5914a1d5fb40f
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-20 12:44:38 +0300

    Titles upd

--- Components/Titles/Titles.tsx
+++ Components/Titles/Titles.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { ITitlesProps } from "./Titles.props";
 
-const Titles = (): JSX.Element => {
-  return <div>Titles</div>;
+const Titles = ({ tag, children }: ITitlesProps): JSX.Element => {
+  return <></>;
 };
 
 export default Titles;
