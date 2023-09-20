2628a93fc3441a73735721c1fd02c82ff2ce7b3e
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-20 13:01:37 +0300

    upd

--- Components/Titles/Titles.tsx
+++ Components/Titles/Titles.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import { ITitlesProps } from "./Titles.props";
 
 const Titles = ({ tag, children }: ITitlesProps): JSX.Element => {
-  return <h1>{children}</h1>;
+  switch (tag) {
+    case "h1":
+      return <h1>{children}</h1>;
+    case "h2":
+      return <h2>{children}</h2>;
+    case "h3":
+      return <h3>{children}</h3>;
+    default:
+      return <></>;
+  }
 };
 
 export default Titles;
