39df9b3069290635eae9d5faa8ea20db8a5960b6
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-21 14:38:00 +0300

    upd

--- Components/Button/Button.tsx
+++ Components/Button/Button.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import styles from "./Button.module.scss";
+import cn from "classnames";
 import { IButton } from "./Button.props";
 
 const Button = ({ appearence, children }: IButton): JSX.Element => {
-  return <button>{children}</button>;
+  return (
+    <button
+      className={cn(styles.button, {
+        [styles.primary]: appearence == "primary",
+        [styles.ghost]: appearence == "ghost",
+      })}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Titles from "@/Components/Titles/Titles";
+import Button from "@/Components/Button/Button";
 
 export default function Home(): JSX.Element {
   return (
     <div>
       <Titles tag='h1'>Text</Titles>
+      <Button appearence='primary'>Primary</Button>
+      <Button appearence='ghost'>Ghost</Button>
     </div>
   );
 }
