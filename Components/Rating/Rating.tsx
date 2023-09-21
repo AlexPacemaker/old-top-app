ebaaa7533603eabfaf3feb6df57d1fd9f594a5b0
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-21 15:33:54 +0300

    rating css upd

--- Components/Rating/Rating.tsx
+++ Components/Rating/Rating.tsx
@@ -1,10 +1,1 @@
-import React from "react";
-import styles from "./Rating.module.scss";
-import cn from "classnames";
-import { IRating } from "./Rating.props";
 
-const Paragraph = ({ isEditable = false, rating, setRating, ...props }: IRating): JSX.Element => {
-  return <div {...props}></div>;
-};
-
-export default Paragraph;
