db122b4c56749b3b578656583c8284e292c5a987
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-22 12:43:30 +0300

    footer done

--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -1,6 +1,8 @@
 import { ILayout } from "./Layout.props";
 import styles from "./Layout.module.scss";
 import cn from "classnames";
+import { Header } from "./Header/Header";
+import { Sidebar } from "./Sidebar/Sidebar";
 
 export const Layout = ({ children }: ILayout): JSX.Element => {
   return (
