ec9fdebd39eff5f893a1e037014b63911ffdadce
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-25 13:47:10 +0300

    footer grid adaptive done

--- Layout/Footer/Footer.tsx
+++ Layout/Footer/Footer.tsx
@@ -1,11 +1,18 @@
 import styles from "./Footer.module.scss";
 import cn from "classnames";
 import { IFooter } from "./Footer.props";
+import { format } from "date-fns";
 
 export const Footer = ({ className, ...props }: IFooter): JSX.Element => {
   return (
     <footer className={cn(className, styles.footer)} {...props}>
-      <div>Все права защищены {}</div>
+      <div>Все права защищены {format(new Date(), "yyyy")}</div>
+      <a href='#' target='_blank'>
+        Пользовательское соглашение
+      </a>
+      <a href='#' target='_blank'>
+        Политика конфиденциальности
+      </a>
     </footer>
   );
 };
