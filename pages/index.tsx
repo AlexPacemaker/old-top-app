fa578a487c62d6de9bb0e8ed4e4120337429ed99
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-26 14:51:00 +0300

    upd

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import Paragraph from "@/Components/Paragraph/Paragraph";
 import Tag from "@/Components/Tag/Tag";
 import Rating from "@/Components/Rating/Rating";
 import { withLayout } from "@/Layout/Layout";
+import { GetStaticProps } from "next";
+import axios from "axios";
 
 const Home = (): JSX.Element => {
   const [rating, setRating] = useState<number>(4);
@@ -35,3 +37,15 @@ const Home = (): JSX.Element => {
 };
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps = async () => {
+  const firstCategory: 0;
+  const { data: menu } = await axios.post(process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find");
+
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
