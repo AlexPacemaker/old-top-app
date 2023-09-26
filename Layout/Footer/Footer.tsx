2e9950da6c4e436271aeb8662fc8c1669b7dc032
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-26 14:23:58 +0300

    upd

--- Layout/Footer/Footer.tsx
+++ Layout/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import { format } from "date-fns";
 export const Footer = ({ className, ...props }: IFooter): JSX.Element => {
   return (
     <footer className={cn(className, styles.footer)} {...props}>
-      <div>Pacenamer © {format(new Date(), "yyyy")}</div>
+      <div>Pacemaker © {format(new Date(), "yyyy")}</div>
       <a href='#' target='_blank'>
         Пользовательское соглашение
       </a>
