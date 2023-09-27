dfc17b0f72fcb9b8b836c1e9c76d784b5ce59998
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-27 13:07:39 +0300

    upd

--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -1,6 +1,5 @@
 import { ILayout } from "./Layout.props";
 import styles from "./Layout.module.scss";
-import cn from "classnames";
 import { Header } from "./Header/Header";
 import { Sidebar } from "./Sidebar/Sidebar";
 import { Footer } from "./Footer/Footer";
