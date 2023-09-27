3879a9d04bcf9365174d1945ba7994d56b2d84af
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-27 14:03:35 +0300

    upd

--- Layout/Menu/Menu.tsx
+++ Layout/Menu/Menu.tsx
@@ -7,30 +7,46 @@ import BooksIcon from "./icons/books.svg";
 import ProductsIcon from "./icons/products.svg";
 import ServicesIcon from "./icons/services.svg";
 import { TopLevelCategory } from "@/Interfaces/toppage.interface";
+import cn from "classnames";
 
-const firstLevelMenu: FirtsLevelMenuItem = [
+const firstLevelMenu: FirtsLevelMenuItem[] = [
   { route: "courses", name: "Курсы", icon: <CoursesIcon />, id: TopLevelCategory.Courses },
   { route: "books", name: "Книги", icon: <BooksIcon />, id: TopLevelCategory.Books },
   { route: "services", name: "Сервисы", icon: <ServicesIcon />, id: TopLevelCategory.Services },
   { route: "products", name: "Продукты", icon: <ProductsIcon />, id: TopLevelCategory.Products },
 ];
 
-const buildFirstLevel = () => {};
-
-const buildSecondLevel = () => {};
-
-const buildThirdLevel = () => {};
-
 export const Menu = (): JSX.Element => {
   const { menu, setMenu, firstCategory } = useContext(AppContext);
 
+  const buildFirstLevel = () => {
+    return (
+      <>
+        {firstLevelMenu.map((menu) => (
+          <div key={menu.id}>
+            <a href={`/${menu.route}`}>
+              <div
+                className={cn(styles.firstLevel, {
+                  [styles.firstLevelActive]: menu.id == firstCategory,
+                })}
+              >
+                {menu.icon}
+                <span>{menu.name}</span>
+              </div>
+            </a>
+          </div>
+        ))}
+      </>
+    );
+  };
+
+  const buildSecondLevel = () => {};
+
+  const buildThirdLevel = () => {};
+
   return (
     <div className={styles.menu}>
-      <ul>
-        {menu.map((m) => (
-          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
-        ))}
-      </ul>
+      <ul>{buildFirstLevel()}</ul>
     </div>
   );
 };
