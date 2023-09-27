cb65e5bfe4aa89cddda979095ed1ffe9fbb68d62
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-27 18:44:04 +0300

    sidebar css

--- Layout/Sidebar/Sidebar.tsx
+++ Layout/Sidebar/Sidebar.tsx
@@ -2,10 +2,13 @@ import { ISidebar } from "./Sidebar.props";
 import styles from "./Sidebar.module.scss";
 import cn from "classnames";
 import { Menu } from "../Menu/Menu";
+import Logo from "../projectlogo.svg";
 
-export const Sidebar = ({ ...props }: ISidebar): JSX.Element => {
+export const Sidebar = ({ className, ...props }: ISidebar): JSX.Element => {
   return (
-    <div {...props}>
+    <div className={cn(className, styles.sidebar)} {...props}>
+      <Logo className={styles.logo} />
+      <div>поиск...</div>
       <Menu />
     </div>
   );
