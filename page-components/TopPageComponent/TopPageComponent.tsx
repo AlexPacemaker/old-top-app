c023b9b48445b70415de3c511c821ddce1e8b2eb
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-28 14:57:20 +0300

    upd

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { TopPageComponentProps } from "./TopPage.props";
+import Tag from "@/Components/Tag/Tag";
+import Titles from "@/Components/Titles/Titles";
 
 const TopPageComponent = ({ firstCategory, page, products }: TopPageComponentProps): JSX.Element => {
-  return <>{products && products.length}</>;
+  return (
+    <>
+      <div>
+        <Titles tag='h1'>{page.title}</Titles>
+        {products && <Tag>{products.length}</Tag>}
+      </div>
+      {products && products.length}
+    </>
+  );
 };
 
 export default TopPageComponent;
