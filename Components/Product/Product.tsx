38713b4c9133973a54d9ae1b917771246b13342d
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-10-05 15:44:41 +0300

    upd

--- Components/Product/Product.tsx
+++ Components/Product/Product.tsx
@@ -3,6 +3,8 @@ import styles from "./Product.module.scss";
 import cn from "classnames";
 import { ProductProps } from "./Product.props";
 import Card from "../Card/Card";
+import Rating from "../Rating/Rating";
+import Tag from "../Tag/Tag";
 
 const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
   return (
@@ -11,6 +13,21 @@ const Product = ({ product, className, ...props }: ProductProps): JSX.Element =>
         <img src={product.image} alt={product.title} />
       </div>
       <div className={styles.title}>{product.title}</div>
+      <div className={styles.price}>{product.price}</div>
+      <div className={styles.credit}>{product.credit}</div>
+      <div className={styles.rating}>
+        <Rating rating={product.reviewAvg ?? product.initialRating} />
+      </div>
+      <div className={styles.tags}>
+        {product.categories.map((categorie) => (
+          <Tag color='ghost' key={categorie}>
+            {categorie}
+          </Tag>
+        ))}
+      </div>
+      <div className={styles.priceTitle}>цена</div>
+      <div className={styles.creditTitle}>кредит</div>
+      <div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
     </Card>
   );
 };
