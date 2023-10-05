d2c25163328fe725206be73428dd0af0379c49b5
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-10-05 15:33:10 +0300

    upd

--- Components/Product/Product.tsx
+++ Components/Product/Product.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import styles from "./Product.module.scss";
 import cn from "classnames";
 import { ProductProps } from "./Product.props";
+import Card from "../Card/Card";
 
 const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
-  return <div>{product.title}</div>;
+  return (
+    <Card>
+      <div className={styles.logo}>
+        <img src={product.image} alt={product.title} />
+      </div>
+      <div className={styles.title}>{product.title}</div>
+    </Card>
+  );
 };
 
 export default Product;
