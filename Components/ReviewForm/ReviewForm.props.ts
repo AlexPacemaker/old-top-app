e0a9340489f63d411207a98806c064f656995681
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-10-06 13:26:27 +0300

    form props done

--- Components/ReviewForm/ReviewForm.props.ts
+++ Components/ReviewForm/ReviewForm.props.ts
@@ -1,6 +1,5 @@
 import { DetailedHTMLProps, HTMLAttributes } from "react";
-import { ReviewModel } from "@/Interfaces/product.interface";
 
-export interface ReviewProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-  review: ReviewModel;
+export interface ReviewFormProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  productId: string;
 }
