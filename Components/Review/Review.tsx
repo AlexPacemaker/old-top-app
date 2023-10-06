760697124dff463c86ae21ea807cb35c6ff07332
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-10-06 12:44:07 +0300

    svg user added, css init

--- Components/Review/Review.tsx
+++ Components/Review/Review.tsx
@@ -2,13 +2,10 @@ import React from "react";
 import styles from "./Review.module.scss";
 import cn from "classnames";
 import { ReviewProps } from "./Review.props";
+import UserIcon from "./user.svg";
 
-const Review = ({ className, ...props }: ReviewProps): JSX.Element => {
-  return (
-    <div className={cn(styles.card, className, styles[color])} {...props}>
-      {children}
-    </div>
-  );
+const Review = ({ review, className, ...props }: ReviewProps): JSX.Element => {
+  return <div className={cn(styles.review, className)} {...props}></div>;
 };
 
 export default Review;
