9456fcfb88dbcba873499c90c01ba4d788ab4cb6
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-10-06 15:29:14 +0300

    input upd forwardref

--- Components/Input/Input.tsx
+++ Components/Input/Input.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+/* eslint-disable react/display-name */
+import React, { ForwardedRef } from "react";
 import styles from "./Input.module.scss";
 import cn from "classnames";
 import { InputProps } from "./Input.props";
 
-const Input = ({ className, ...props }: InputProps): JSX.Element => {
-  return <input className={cn(className, styles.input)} {...props} />;
-};
+const Input = React.forwardRef(
+  ({ className, ...props }: InputProps, ref: ForwardedRef<HTMLInputElement>): JSX.Element => {
+    return <input className={cn(className, styles.input)} ref={ref} {...props} />;
+  }
+);
 
 export default Input;
