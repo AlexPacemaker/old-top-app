99c7524081151f8377c4d6d02cede5d6f264ada8
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-10-06 12:35:56 +0300

    review init code and css

--- Components/Product/Product.tsx
+++ Components/Product/Product.tsx
@@ -11,71 +11,87 @@ import Divider from "../Divider/Divider";
 import Image from "next/image";
 
 const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
+  const [isReviewOpened, setIsReviewOpened] = React.useState<boolean>(false);
+
   return (
-    <Card className={styles.product}>
-      <div className={styles.logo}>
-        <Image src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} width={70} height={70} />
-      </div>
-      <div className={styles.title}>{product.title}</div>
-      <div className={styles.price}>
-        {priceRu(product.price)}
-        {product.oldPrice && (
-          <Tag className={styles.oldPrice} color='green'>
-            {priceRu(product.price - product.oldPrice)}
-          </Tag>
-        )}
-      </div>
-      <div className={styles.credit}>
-        {priceRu(product.credit)}/<span className={styles.month}>мес.</span>
-      </div>
-      <div className={styles.rating}>
-        <Rating rating={product.reviewAvg ?? product.initialRating} />
-      </div>
-      <div className={styles.tags}>
-        {product.categories.map((categorie) => (
-          <Tag className={styles.category} color='ghost' key={categorie}>
-            {categorie}
-          </Tag>
-        ))}
-      </div>
-      <div className={styles.priceTitle}>цена</div>
-      <div className={styles.creditTitle}>кредит</div>
-      <div className={styles.rateTitle}>
-        {product.reviewCount} {declOfNum(product.reviewCount, ["отзыв", "отзыва", "отзывов"])}
-      </div>
-      <Divider className={styles.hr} />
-      <div className={styles.description}>{product.description}</div>
-      <div className={styles.feature}>
-        {product.characteristics.map((char) => (
-          <div className={styles.characteristics} key={char.name}>
-            <span className={styles.characteristicsName}>{char.name}</span>
-            <span className={styles.characteristicsDots}></span>
-            <span className={styles.characteristicsValue}>{char.value}</span>
-          </div>
-        ))}
-      </div>
-      <div className={styles.advBlock}>
-        {product.advantages && (
-          <div className={styles.advantages}>
-            <div className={styles.advTitle}>Преимущества</div>
-            <div>{product.advantages}</div>
-          </div>
-        )}
-        {product.disadvantages && (
-          <div className={styles.disadvantages}>
-            <div>Недостатки</div>
-            <div>{product.disadvantages}</div>
-          </div>
-        )}
-      </div>
-      <Divider className={styles.hr} />
-      <div className={styles.actions}>
-        <Button appearance='primary'>Узнать подробнее</Button>
-        <Button appearance='ghost' arrow={"right"} className={styles.reviewButton}>
-          Читать отзывы
-        </Button>
-      </div>
-    </Card>
+    <>
+      <Card className={styles.product}>
+        <div className={styles.logo}>
+          <Image src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} width={70} height={70} />
+        </div>
+        <div className={styles.title}>{product.title}</div>
+        <div className={styles.price}>
+          {priceRu(product.price)}
+          {product.oldPrice && (
+            <Tag className={styles.oldPrice} color='green'>
+              {priceRu(product.price - product.oldPrice)}
+            </Tag>
+          )}
+        </div>
+        <div className={styles.credit}>
+          {priceRu(product.credit)}/<span className={styles.month}>мес.</span>
+        </div>
+        <div className={styles.rating}>
+          <Rating rating={product.reviewAvg ?? product.initialRating} />
+        </div>
+        <div className={styles.tags}>
+          {product.categories.map((categorie) => (
+            <Tag className={styles.category} color='ghost' key={categorie}>
+              {categorie}
+            </Tag>
+          ))}
+        </div>
+        <div className={styles.priceTitle}>цена</div>
+        <div className={styles.creditTitle}>кредит</div>
+        <div className={styles.rateTitle}>
+          {product.reviewCount} {declOfNum(product.reviewCount, ["отзыв", "отзыва", "отзывов"])}
+        </div>
+        <Divider className={styles.hr} />
+        <div className={styles.description}>{product.description}</div>
+        <div className={styles.feature}>
+          {product.characteristics.map((char) => (
+            <div className={styles.characteristics} key={char.name}>
+              <span className={styles.characteristicsName}>{char.name}</span>
+              <span className={styles.characteristicsDots}></span>
+              <span className={styles.characteristicsValue}>{char.value}</span>
+            </div>
+          ))}
+        </div>
+        <div className={styles.advBlock}>
+          {product.advantages && (
+            <div className={styles.advantages}>
+              <div className={styles.advTitle}>Преимущества</div>
+              <div>{product.advantages}</div>
+            </div>
+          )}
+          {product.disadvantages && (
+            <div className={styles.disadvantages}>
+              <div>Недостатки</div>
+              <div>{product.disadvantages}</div>
+            </div>
+          )}
+        </div>
+        <Divider className={styles.hr} />
+        <div className={styles.actions}>
+          <Button appearance='primary'>Узнать подробнее</Button>
+          <Button
+            appearance='ghost'
+            arrow={isReviewOpened ? "down" : "right"}
+            className={styles.reviewButton}
+            onClick={() => setIsReviewOpened(!isReviewOpened)}
+          >
+            Читать отзывы
+          </Button>
+        </div>
+      </Card>
+      <Card
+        color='blue'
+        className={cn(styles.review, {
+          [styles.opened]: isReviewOpened,
+          [styles.closed]: !isReviewOpened,
+        })}
+      ></Card>
+    </>
   );
 };
 
